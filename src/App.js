1f8ec16257162f467c1c0b415673795c44ba3264
Samxzeng2019 <38000311+Samxzeng@users.noreply.github.com>
2019-08-13 11:26:58 +0800

    strcuture project

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import styles from './App.css';
 function App() {
   return (
     <div className={styles.app}>
-      <header className={styles.header}>
-        This is header
-      </header>
     </div>
   );
 }
